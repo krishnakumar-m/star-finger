ee495b2803da59c435636f6ca9b8e0ce2a8c014c
Krishnakumar Muraleedharan <krishnakumar777@gmail.com>
2016-07-08 22:26:29 +0530

    Draw using sprites and shipType. Means less code.

--- js/Ships.js
+++ js/Ships.js
@@ -31,14 +31,12 @@ function Ship(loc,vel,w,h,life) {
 	this.counter = 0;
 	this.isPlayer = true;
 	this.setWeapon('twingun');
+	this.shipType = 'crosswing';
     }
 
 Ship.prototype.show = function() {
-	var h4 = this.h / 4;
-	var w4 = this.w / 4;
-	space.rect(this.loc.x, this.loc.y, this.w, h4, 'white', 'white');
-	space.rect(this.loc.x + w4, this.loc.y + h4, w4, 2 * h4, 'white', 'blue');
-	space.rect(this.loc.x, this.loc.y + 3 * h4, this.w, h4, 'white', 'white');
+	
+	space.ctx.putImageData(sprites[this.shipType],this.loc.x,this.loc.y);
     };
 Ship.prototype.setWeapon = function(weaponId) {
     this.weapon = weaponId;
@@ -59,6 +57,7 @@ EnemyShip.prototype = Object.create(Ship.prototype);
 function EnemyShip(loc,vel,w,h,life) {
 	Ship.call(this, loc, vel, w, h,life);
 	this.isPlayer = false;
+	this.shipType = 'raider';
 	//this.setWeapon('oneshot');
 	
     }
@@ -77,13 +76,6 @@ EnemyShip.prototype.move = function() {
 	   enemyBullets = enemyBullets.concat(bllts);
 	}
     };
-EnemyShip.prototype.show = function() {
-	var h4 = this.h / 4;
-	var w4 = this.w / 4;
-	space.rect(this.loc.x, this.loc.y, this.w, h4, 'red', 'red');
-	space.rect(this.loc.x + 2 * w4, this.loc.y + h4, w4, 2 * h4, 'white', 'white');
-	space.rect(this.loc.x, this.loc.y + 3 * h4, this.w, h4, 'red', 'red');
-    };
     
 /*
 Tiny ship
@@ -93,47 +85,24 @@ Mouse.prototype = Object.create(EnemyShip.prototype);
 function Mouse(loc,vel,w,h,life) {
 	EnemyShip.call(this, loc, vel, w, h,life);
 	this.setWeapon('oneshot');
+	this.shipType = 'mouse';
 }
 	
-Mouse.prototype.show = function() {
-	var h3 = this.h / 3;
-	var w3 = this.w / 3;
-	space.rect(this.loc.x+w3, this.loc.y, this.w, h3, 'red', 'red');
-	space.rect(this.loc.x, this.loc.y + h3,this.w, h3, 'red', 'red');
-	space.rect(this.loc.x+w3, this.loc.y+2*h3, this.w, h3, 'red', 'red');
-    }; 
-    
 /* Rat - bigger version of mouse */
 Rat.prototype = Object.create(EnemyShip.prototype);
 
 function Rat(loc,vel,w,h,life) {
 	EnemyShip.call(this, loc, vel, w, h,life);
 	this.setWeapon('homer');
+	this.shipType = 'rat';
 }
-	
-Rat.prototype.show = function() {
-	var h3 = this.h / 3;
-	var w3 = this.w / 3;
-	space.rect(this.loc.x+w3, this.loc.y, this.w, h3, 'white', 'white');
-	space.rect(this.loc.x, this.loc.y + h3, this.w, h3, 'red', 'red');
-	space.rect(this.loc.x+w3, this.loc.y+2*h3, this.w, h3, 'white', 'white');
-    }; 
-    
+
 /* Warbird - staircase ship */
 Warbird.prototype = Object.create(EnemyShip.prototype);
 
 function Warbird(loc,vel,w,h,life) {
 	EnemyShip.call(this, loc, vel, w, h,life);
 	this.setWeapon('3shot');
+	this.shipType ='warbird';
 }
 	
-Warbird.prototype.show = function() {
-	var h = this.h / 5;
-	var w = this.w / 5;
-	var w3 = 3*w;
-	space.rect(this.loc.x+2*w, this.loc.y, w3, h, 'white', 'white');
-	space.rect(this.loc.x+w, this.loc.y + h, w3, h, 'red', 'red');
-	space.rect(this.loc.x, this.loc.y+2*h, w3, h, 'white', 'white');
-	space.rect(this.loc.x+w, this.loc.y+3*h, w3, h, 'red', 'red');
-	space.rect(this.loc.x+2*w, this.loc.y + 4*h, w3, h, 'white', 'white');
-    }; 
