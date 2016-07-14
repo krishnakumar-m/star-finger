d6871c29c5fc27e4c18e8e9e7fa85a4d0eb98c8c
Krishnakumar Muraleedharan <krishnakumar777@gmail.com>
2016-07-14 10:15:24 +0530

    sprite loader for ships

--- js/f-shoot.js
+++ js/f-shoot.js
@@ -1,285 +1,73 @@
 // y relative position on y axis
-var shipList = ['mouse','raider','warbird','rat'];
-var sprites = {};
+var shipList = ['crosswing','mouse','raider','warbird','rat'];
 
-function crosswing(w,h) {
-	var cvs = new Canvas('fld', w, h);
-	var unit = w / 18;
-
-	cvs.ctx.fillStyle = 'Gray';
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(11 * unit, unit);
-	cvs.ctx.lineTo(w, unit);
-	cvs.ctx.lineTo(12 * unit, 5 * unit);
-	cvs.ctx.lineTo(6 * unit, 5 * unit);
-	cvs.ctx.closePath();
-	cvs.ctx.stroke();
-	cvs.ctx.fill();
-
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(11 * unit, 14 * unit);
-	cvs.ctx.lineTo(w, 14 * unit);
-	cvs.ctx.lineTo(12 * unit, 10 * unit);
-	cvs.ctx.lineTo(6 * unit, 10 * unit);
-	cvs.ctx.closePath();
-	cvs.ctx.stroke();
-	cvs.ctx.fill();
-
-	cvs.ctx.fillStyle = 'DarkGray';
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(3 * unit, 5 * unit);
-	cvs.ctx.lineTo(8 * unit, 5 * unit);
-	cvs.ctx.lineTo(4 * unit, unit);
-	cvs.ctx.lineTo(2*unit, 2*unit);
-	cvs.ctx.closePath();
-	cvs.ctx.stroke();
-	cvs.ctx.fill();
-
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(3 * unit, 10 * unit);
-	cvs.ctx.lineTo(8 * unit, 10 * unit);
-	cvs.ctx.lineTo(4 * unit, 14 * unit);
-	cvs.ctx.lineTo(2*unit, 13 * unit);
-	cvs.ctx.closePath();
-	cvs.ctx.stroke();
-	cvs.ctx.fill();
-
-	cvs.roundRect(2 * unit, 6 * unit, 16 * unit, 3 * unit, unit, 'Gray', 'Black');
-	cvs.roundRect(3 * unit, 5 * unit, 9 * unit, 2 * unit, unit, 'Crimson', 'Black');
-	cvs.roundRect(3 * unit, 8 * unit, 9 * unit, 2 * unit, unit, 'Crimson', 'Black');
-
-	cvs.rect(0, 6.5 * unit, 2 * unit, 2 * unit, 'Black', 'White');
-
-	for(var y=6.5 * unit;y < 8.5 * unit;y += unit / 4) {
-		cvs.ctx.moveTo(unit / 2, y);
-		cvs.ctx.lineTo(2 * unit, y);
-		cvs.ctx.stroke();
-	    }
-
-
-	cvs.roundRect(unit, unit, 4 * unit, unit, unit / 2, 'red', 'black');
-	cvs.roundRect(unit, h - 2 * unit, 4 * unit, unit, unit / 2, 'red', 'black');
-	cvs.roundRect(10 * unit, 0, 8 * unit, 2 * unit, unit, 'red', 'black');
-	cvs.roundRect(10 * unit, h - 2 * unit, 8 * unit, 2 * unit, unit, 'red', 'black');
-
-	cvs.roundRect(12 * unit, 6.5 * unit, 4 * unit, 2 * unit, unit, 'white', 'black');
-	
-	sprites['crosswing'] = cvs.ctx.getImageData(0,0,w,h);
-    }
-    
-    
-function mouse(w,h) {
-	var cvs = new Canvas('fld',w,h);;
-	var ctx = cvs.ctx;
-
-	var r = w / 5;
-	var grd = ctx.createRadialGradient(4*r,r,r/2,4*r,r,r);
-	grd.addColorStop(0,'White');
-	grd.addColorStop(0.5,'DarkGray');
-	grd.addColorStop(1,'White');
-	ctx.fillStyle = 'Gray';
-	ctx.strokeStyle = 'Black';
-	ctx.beginPath();
-	var startAngle = 0.75 * Math.PI;
-	var endAngle = 1.25 * Math.PI;
-	var innerRadRatio = 0.25;
-	var x1 = 4 * r + r * Math.cos(startAngle);
-	var x2 = 4 * r + r * Math.cos(startAngle) * innerRadRatio;
-	var y1 = r - r * Math.sin(startAngle);
-	var y2 = r - r * Math.sin(startAngle) * innerRadRatio;
-	var y3 = r - r * Math.sin(endAngle);
-	var y4 = r - r * Math.sin(endAngle) * innerRadRatio;
-
-	ctx.arc(4 * r, r, r, endAngle, startAngle);
-	ctx.arc(4 * r, r, r * innerRadRatio, startAngle, endAngle, true);
-	ctx.closePath();
-	//ctx.fill();
-        grd = ctx.createLinearGradient(0,y1,0,y2-y1);
-	grd.addColorStop(0,'White');
-	grd.addColorStop(0.5,'DarkGray');
-	grd.addColorStop(1,'White');
-	//ctx.fillStyle = grd;
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(r, y1);
-	ctx.lineTo(0, y2);
-	ctx.lineTo(x2, y2);
-	ctx.closePath();
-      //  ctx.fill();
-	ctx.moveTo(x1, y3);
-	ctx.lineTo(r, y3);
-	ctx.lineTo(0, y4);
-	ctx.lineTo(x2, y4);
-	ctx.closePath();
-	ctx.stroke();
-	ctx.fill();
-
-        sprites['mouse'] = ctx.getImageData(0,0,w,h);
-    }
-function raider(w,h) {
-	var unit = w / 4;
-	var cvs = new Canvas('fld', w, h);
-	cvs.ctx.strokeStyle = 'Gray';
-	var tmp = cvs.ctx.lineWidth;
-	cvs.ctx.lineWidth = 5;
-	cvs.ctx.moveTo(3 * unit, unit / 2);
-	cvs.ctx.lineTo(3 * unit, 2.5 * unit + 1);
-	cvs.ctx.stroke();
-
-	cvs.ctx.moveTo(2.5 * unit, unit / 2);
-	cvs.ctx.lineTo(3.5 * unit, 2.5 * unit + 1);
-	cvs.ctx.stroke();
-
-	cvs.ctx.moveTo(3.5 * unit, unit / 2);
-	cvs.ctx.lineTo(2.5 * unit, 2.5 * unit + 1);
-	cvs.ctx.stroke();
-
-	cvs.ctx.lineWidth = tmp;
-	cvs.circle(3 * unit, 1.5 * unit, unit / 2, 'Black', 'Red');
-	cvs.roundRect(0, 1.33 * unit, 3 * unit, unit / 3, unit / 10, 'Gray', 'Black');
-
-	cvs.ctx.strokeStyle = 'Black';
-	cvs.ctx.fillStyle = 'Gray';
-
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(0, unit / 2);
-	cvs.ctx.quadraticCurveTo(0, 0, unit, 0);
-	cvs.ctx.lineTo(3 * unit, 0);
-	cvs.ctx.quadraticCurveTo(w, 0, w, unit / 2);
-	cvs.ctx.closePath();
-	cvs.ctx.stroke();
-	cvs.ctx.fill();
-
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(0, 2.5 * unit);
-	cvs.ctx.quadraticCurveTo(0, 3 * unit, unit, 3 * unit);
-	cvs.ctx.lineTo(3 * unit, 3 * unit);
-	cvs.ctx.quadraticCurveTo(w, 3 * unit, w, 2.5 * unit);
-	cvs.ctx.closePath();
-	cvs.ctx.stroke();
-	cvs.ctx.fill();
-
-	cvs.ctx.strokeStyle = 'rgb(255,255,255,0)';
-	for(var x=unit / 10;x < unit;x += unit / 10) {
-		cvs.ctx.moveTo(x, 1.33 * unit);
-		cvs.ctx.lineTo(x, 1.66 * unit);
-		cvs.ctx.stroke();
-	    }
-        sprites['raider'] = cvs.ctx.getImageData(0,0,w,h);
-    }
-    
-    
-//5:7    
-function warbird(w,h) {
-	var cvs = new Canvas('fld', w, h);
-
-
-	//var w = 250, h = 350;
-	var unit = w / 5;
-	cvs.ctx.translate(w, h);
-	cvs.ctx.rotate(Math.PI);
-	cvs.rect(1.5 * unit, 1.66 * unit, unit, 3.66 * unit, 'Gray', 'Gray');
-	cvs.roundRect(unit, 2 * unit, 2 * unit, 3 * unit, unit / 5, 'White', 'Black');
-	cvs.roundRect(2 * unit, 3 * unit, 2 * unit, unit, unit / 10, 'Gray', 'Black');
-	cvs.rect(4 * unit, 3.33 * unit, unit, 0.33 * unit, 'Black', 'White');
-	cvs.ctx.strokeStyle = 'rgb(255,255,255,0)';
-	for(var x=4 * unit;x < 5 * unit;x += unit / 10) {
-		cvs.ctx.moveTo(x, 3.33 * unit);
-		cvs.ctx.lineTo(x, 3.66 * unit);
-		cvs.ctx.stroke();
-	    }
-
-	cvs.ctx.fillStyle = 'Gray';
-
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(2 * unit, 0);
-	cvs.ctx.lineTo(4 * unit, 0);
-	cvs.ctx.lineTo(5 * unit, unit);
-	cvs.ctx.lineTo(5 * unit, 1.66 * unit);
-	cvs.ctx.lineTo(0, 1.66 * unit);
-	cvs.ctx.closePath();
-	cvs.ctx.fill();
-	cvs.ctx.stroke();
-	cvs.ctx.beginPath();
-	cvs.ctx.moveTo(2 * unit, h);
-	cvs.ctx.lineTo(4 * unit, h);
-	cvs.ctx.lineTo(5 * unit, h - unit);
-	cvs.ctx.lineTo(5 * unit, h - 1.66 * unit);
-	cvs.ctx.lineTo(0, h - 1.66 * unit);
-	cvs.ctx.closePath();
-
-
-	cvs.ctx.fill();
-	cvs.ctx.stroke();
-        sprites['warbird'] = cvs.ctx.getImageData(0,0,w,h);
-    }
-    
-    function rat(w,h) {
-    var cvs = new Canvas('fld', w, h);
-    var unit = w/7;
-    
-    var ctx = cvs.ctx;
-ctx.beginPath();
-//ctx.moveTo(2.5*unit, h/2);
-//ctx.quadraticCurveTo(20, 100, 200, 20);
-var grd = ctx.createLinearGradient(0,0,w,0);
-grd.addColorStop(0,"gray");
-grd.addColorStop(1,"white");
-//ctx.fillStyle='#616362';
-ctx.fillStyle=grd;
-ctx.moveTo(0, 3*unit);
-ctx.lineTo(3*unit, 3*unit);
-ctx.quadraticCurveTo(0.6*w, 0, w, 0);
-ctx.quadraticCurveTo(w/9, 0,0, 3*unit);
-ctx.stroke();
-ctx.moveTo(0, 4*unit);
-ctx.lineTo(3*unit, 4*unit);
-ctx.quadraticCurveTo(0.6*w, h, w, h);
-ctx.quadraticCurveTo(w/9, h,0, 4*unit);
-ctx.stroke();
-ctx.fill();
-
-
-grd = ctx.createRadialGradient(unit,h/2,unit/3,unit,h/2,unit);
-grd.addColorStop(0,"red");
-//grd.addColorStop(0.5,"")
-grd.addColorStop(1,"gray");
-
-//ctx.fillStyle='red';
-ctx.fillStyle=grd;
-ctx.beginPath();
-ctx.arc(1.5*unit,h/2,unit/2,0,2*Math.PI);
-ctx.stroke();
-ctx.fill();
-sprites['rat'] = cvs.ctx.getImageData(0,0,w,h);
-}
-    
 var ships = {
+    'crosswing' : {
+	w : 48,
+	h : 40,
+	hp : 60,
+	vel : new Point(0, 0),
+	spriteMaker : crosswing
+	},
     'mouse' : {
 	w : 30,
 	h : 12,
 	hp : 5,
-	vel : new Point(-4, 0)
+	vel : new Point(-4, 0),
+	spriteMaker : mouse
 	},
     'raider' : {
 	w : 40,
 	h : 30,
 	hp : 15,
-	vel : new Point(-3, 0)
+	vel : new Point(-3, 0),
+	spriteMaker : raider
 	},
     'warbird' : {
 	w : 50,
 	h : 70,
 	hp : 25,
-	vel : new Point(-2, 0)
+	vel : new Point(-2, 0),
+	spriteMaker : warbird
 	},
     'rat' : {
 	w : 20,
 	h : 20,
 	hp : 10,
-	vel : new Point(-4, 0)
+	vel : new Point(-4, 0),
+	spriteMaker : rat
 	}
     };
+var sprites = {};
+
+
+function preLoadShipSprites() {
+    var len = shipList.length;
     
+    for(var i=0;i<len;i++) {
+	var thisShipType = shipList[i];
+	ships[thisShipType].sprite = getSprites(thisShipType);
+    }
+}
+
+function getSprites(id) {
+    var w = ships[id].w;
+    var h = ships[id].h;
+    var cvs = new Canvas('fld', w, h);
+    
+    ships[id].spriteMaker(w,h,cvs);
+    
+    var img = new Image();
+    img.src = cvs.cvs.toDataURL('img/png');
+    
+    return img;
+}
+
+
+
+
+
+
 
 var waves = {
     50 : [ {
@@ -347,7 +135,7 @@ var waves = {
 	    ship : 'rat'
 	    }
 	],
-      250:  [{
+    250:  [{
 	    y : 0.5,
 	    ship : 'warbird'
 	    }
@@ -372,7 +160,7 @@ var waves = {
 	]
 
     };
-    
+
 var endless = true;//Endless Random Hell
 var enemies = [], bullets = [], enemyBullets = [], powerUps=[];
 var hs = [0,60,120];
@@ -414,17 +202,18 @@ function test() {
 	 src: randomStarBg()
 	 }
 	 */
-mouse(ships.mouse.w,ships.mouse.h);
-crosswing(42,35);
-raider(ships.raider.w,ships.raider.h);
-warbird(ships.warbird.w,ships.warbird.h);
-rat(ships.rat.w,ships.rat.h);
+	/*mouse(ships.mouse.w, ships.mouse.h);
+	crosswing(48, 40);
+	raider(ships.raider.w, ships.raider.h);
+	warbird(ships.warbird.w, ships.warbird.h);
+	rat(ships.rat.w, ships.rat.h);*/
+	preLoadShipSprites();
 	space = new Canvas('fld', window.innerWidth, window.innerHeight);
 
-	player = new Ship(new Point(0, 0), new Point(0, 0), 42, 35, maxLife);
+	player = new Ship(new Point(0, 0), new Point(0, 0), 48, 40, maxLife);
 	Scenery.init(bgs);
-	
-	
+
+
 
 	var shipControl = false;
 
@@ -652,25 +441,25 @@ function newShips() {
 			    }
 			enemies.push(shipFactory(params));
 		    }
-		  nextWaveIfEndless();  
+		nextWaveIfEndless();  
 	    }
     }
-	    
-	function nextWaveIfEndless() {
-	           if(endless) {
-		    waves = {};
-		    var nextTime = levelTimer+getRandomInt(50,100);
-		    var nextWave = [], shipObj;
-		    var nShips = getRandomInt(1,10);
-		    
-		    var interval = 1/(nShips+1);
-		    var nAvailShips = shipList.length;
-		    for(i=1;i<=nShips;i++) {
+
+function nextWaveIfEndless() {
+	if(endless) {
+		waves = {};
+		var nextTime = levelTimer + getRandomInt(50, 100);
+		var nextWave = [], shipObj;
+		var nShips = getRandomInt(1, 10);
+
+		var interval = 1 / (nShips + 1);
+		var nAvailShips = shipList.length;
+		for(i = 1;i <= nShips;i++) {
 			shipObj = {};
 			shipObj.y = interval * i;
-			shipObj.ship = shipList[getRandomInt(0,nAvailShips-1)];
+			shipObj.ship = shipList[getRandomInt(1, nAvailShips - 1)];
 			nextWave.push(shipObj);
 		    }
-		    waves[nextTime] = nextWave;
-		}
-	} 
+		waves[nextTime] = nextWave;
+	    }
+    } 
